3dd3e91129d423f724c1935e4a0995789241deb9
EZIO AUDITORE <ezioauditoredefinerz@gmail.com>
2025-06-29 01:15:53 +0530

    first commit

---
+++ __settings__.js
@@ -0,0 +1,23 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2248677.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': false,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 231423546, 231423542, 231423517, 231423540, 231423522, 231423575, 231423514, 231423611, 231423543, 231423573, 231423544, 231423545, 231423566, 231612734, 232172628, 232193591, 232193592, 233611025 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/231423524/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/231423531/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/231423529/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/231423526/1/basis.wasm.js', 'wasmUrl' : 'files/assets/231423535/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/231423539/1/basis.js', 'preload' : false},
+];
